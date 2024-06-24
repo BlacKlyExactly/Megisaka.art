69c1c1d106dfb8cbdf6a8524585d89b65ab74777
BlacKlyExactly <supreme24d@gmail.com>
2024-06-24 13:02:07 +0200

    fix images

--- src/app/[lang]/portfolio/page.tsx
+++ src/app/[lang]/portfolio/page.tsx
@@ -5,9 +5,9 @@ const Portfolio = () => (
     <WorkShowcase
       className="mt-10"
       works={[
-        { title: 'Megura', type: 'VTuber', image: '/image1.png' },
-        { title: 'Megura1', type: 'VTuber', image: '/image2.png' },
-        { title: 'Megura2', type: 'VTuber', image: '/image3.png' },
+        { title: 'Megura', type: 'VTuber', image: '/images/image1.png' },
+        { title: 'Megura1', type: 'VTuber', image: '/images/image2.png' },
+        { title: 'Megura2', type: 'VTuber', image: '/images/image3.png' },
       ]}
     />
   </main>
