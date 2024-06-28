887d109c58353de8ea150484fcf0634dd919b24c
BlacKlyExactly <supreme24d@gmail.com>
2024-06-28 17:31:31 +0200

    Add vercel speed insights

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,5 +1,6 @@
 import '../globals.css';
 import type { Metadata } from 'next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Toaster } from 'react-hot-toast';
 
 import Nav from '@/components/nav/Nav';
@@ -39,6 +40,7 @@ export default async function RootLayout({
         <AnimatedCursor />
         <PageTransition />
         <SmoothScrollbar />
+        <SpeedInsights />
         <Toaster />
         <Nav nav={nav} lang={lang} />
 
