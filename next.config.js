a22417543f3c00e3b9533f735cfac166471eb137
BlacKlyExactly <supreme24d@gmail.com>
2024-10-11 13:04:53 +0200

    Increase body size limit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb',
+    },
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
